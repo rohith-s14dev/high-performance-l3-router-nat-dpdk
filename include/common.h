c64dec5918c04dc96528ad3c6f1bb55f1d109adb
Rohith S <126876994+rohith-s14dev@users.noreply.github.com>
2026-09-09 17:59:23 +0530

    Add common DPDK definitions

---
+++ include/common.h
@@ -0,0 +1,21 @@
+#ifndef ROUTER_COMMON_H
+#define ROUTER_COMMON_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <rte_common.h>
+#include <rte_eal.h>
+#include <rte_ethdev.h>
+#include <rte_mbuf.h>
+#include <rte_mempool.h>
+
+#define RX_RING_SIZE 1024
+#define TX_RING_SIZE 1024
+#define NUM_MBUFS 8191
+#define MBUF_CACHE_SIZE 250
+#define BURST_SIZE 32
+
+#endif
